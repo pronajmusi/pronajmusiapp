93b4ab5b582ab82220a4ac8428f915acfb491737
Martin <martinbalajka23@gmail.com>
2023-12-10 14:08:50 +0100

    password input  update

--- src/app/profil/UserProfile.tsx
+++ src/app/profil/UserProfile.tsx
@@ -40,6 +40,7 @@ export default function UserProfile() {
                     className="border mb-4 px-4 py-2 rounded shadow focus:outline-none focus:border-indigo-500 focus:ring-indigo-500 focus:ring-1"
                 />
             </div>
+
             <div className="flex flex-col w-full mb-4">
                 <span className="text-lg mb-4">Uživatelské jméno</span>
                 <input
@@ -51,21 +52,25 @@ export default function UserProfile() {
                 />
             </div>
 
-            <div className="flex flex-row w-full mb-4 items-center">
-                <input
-                    value={newPassword}
-                    onChange={(e) => setNewPassword(e.target.value)}
-                    type="password"
-                    placeholder="Nové heslo"
-                    className="border px-4 py-2 rounded-l shadow focus:outline-none focus:border-indigo-500 focus:ring-indigo-500 focus:ring-1 flex-grow mr-2"
-                />
-                <button
-                    onClick={onChangePasswordClick}
-                    className="text-white bg-black px-4 py-2 cursor-pointer rounded-r hover:bg-indigo-700 duration-150"
-                >
-                    Změnit heslo
-                </button>
+            <div className="flex flex-col w-full mb-4">
+                <span className="text-lg mb-2">Heslo</span>
+                <div className="flex flex-row items-center">
+                    <input
+                        value={newPassword}
+                        onChange={(e) => setNewPassword(e.target.value)}
+                        type="password"
+                        placeholder="Nové heslo"
+                        className="border px-4 py-2 rounded-l shadow focus:outline-none focus:border-indigo-500 focus:ring-indigo-500 focus:ring-1 flex-grow mr-2"
+                    />
+                    <button
+                        onClick={onChangePasswordClick}
+                        className="text-white bg-black px-4 py-2 cursor-pointer rounded-r hover:bg-indigo-700 duration-150"
+                    >
+                        Změnit heslo
+                    </button>
+                </div>
             </div>
+
             <div className="flex flex-col w-full mb-4">
                 <button
                     onClick={() => onSignOut()}
